4b8fe9326ad293b2506c669ffc7d00a7e589cd96
Wen Hao <whlui@stanford.edu>
2014-03-02 04:36:56 -0800

    eye corner detection works

--- detectEyeShape.py
+++ detectEyeShape.py
@@ -1,5 +1,5 @@
 from imports import *
-from util import rotate2d
+from util import rotate2d, rotZ
 import math
 
 if len(sys.argv) < 2:
@@ -18,35 +18,54 @@ numLines = origImg.shape[0]
 startLine = int(numLines * 0.25)
 endLine = int(numLines * 0.75)
 origImg = origImg[startLine: endLine]
+
+imgCenter = (origImg.shape[0] / 2, origImg.shape[1] / 2)
+
 img = cv2.GaussianBlur(origImg, (0, 0), 1)
 
+
 newImg = np.zeros(img.shape)
-newImg[img < 48] = 1
+newImg[img < 44] = 1
 
 index = np.vstack(newImg.nonzero()) # 2xM matrix
 
-index[(0,1),:] = index[(1,0),:]
+index[(0, 1), :] = index[(1, 0), :]
 
-index = index[:,index[1].argsort()[::-1]]
+index = index[:, index[1].argsort()[::-1]]
+index = np.concatenate((index,np.ones((1,index.shape[1]),dtype=int)), axis=0)
+
 XmaxIdx = np.argmax(index[0])
 XminIdx = np.argmin(index[0])
 
-
 diff = index[:, XmaxIdx] - index[:, XminIdx]
 
-angle = math.atan2(diff[0], diff[1])
-rotIdx = rotate2d(angle).dot(index)
-YmaxIdx = np.argmax(rotIdx[1])
-YminIdx = np.argmin(rotIdx[1])
-Xavg = (index[0,XmaxIdx] + index[0,XminIdx]) / 2
+angle = math.degrees( math.atan2(diff[1], diff[0] ))
 
-index = index.T
+print angle
+rotation = cv2.getRotationMatrix2D(imgCenter, angle, 1)
+rotIdx = rotation.dot(index)
+
+rotImg = cv2.warpAffine(img, rotation, img.shape[::-1])
+yMax = np.max(rotIdx[1])
+yMin = np.min(rotIdx[1])
+
+rotXavg = (rotIdx[0, XmaxIdx] + rotIdx[0, XminIdx]) / 2.
+yOrigPoints = np.array([
+    [rotXavg, rotXavg],
+    [yMin, yMax],
+    [1,1],
+])
+yPoints = cv2.getRotationMatrix2D(imgCenter, -angle, 1).dot(yOrigPoints).astype(int)
+
+yOrigPoints = yOrigPoints[:2].astype(int)
+cv2.circle(rotImg, tuple(yOrigPoints[:,0]), 2, (0, 255, 0))
+cv2.circle(rotImg, tuple(yOrigPoints[:,1]), 2, (0, 255, 0))
 
 keypoints = [
-    tuple(index[XminIdx]),
-    tuple(index[XmaxIdx]),
-    (Xavg, index[YmaxIdx, 1]),
-    (Xavg, index[YminIdx, 1]),
+    tuple(index[:2,XminIdx]),
+    tuple(index[:2,XmaxIdx]),
+    tuple(yPoints[:, 0]),
+    tuple(yPoints[:, 1]),
 ]
 
 print keypoints
@@ -54,8 +73,9 @@ print keypoints
 for x in keypoints:
     cv2.circle(img, x, 2, (0, 255, 0))
 
-plt.subplot(121), plt.imshow(img, cmap=cm.Greys_r)
-plt.subplot(122), plt.imshow(newImg)
-# plt.subplot(223)
+
+plt.subplot(221), plt.imshow(img, cmap=cm.Greys_r)
+plt.subplot(222), plt.imshow(newImg)
+plt.subplot(223), plt.imshow(rotImg)
 
 plt.show()
