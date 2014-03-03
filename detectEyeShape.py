5e62074d2d48d2fe859e471428f00fe1c163cae1
Wen Hao <whlui@stanford.edu>
2014-03-03 02:22:00 -0800

    extracted features

--- detectEyeShape.py
+++ detectEyeShape.py
@@ -1,107 +1,116 @@
 from imports import *
-from util import rotate2d, rotZ
 import math
-
-if len(sys.argv) < 2:
-    print 'Usage: python ' + __file__ + ' <image>'
-    sys.exit()
-
 
 def nothing(x):
     pass
 
+def getEyeFeatures(origImg, verbose):
 
-imgFile = sys.argv[1]
-origImg = cv2.imread(imgFile, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-numLines = origImg.shape[0]
+    result = {}
+    numLines = origImg.shape[0]
+    startLine = int(numLines * 0.25)
+    endLine = int(numLines * 0.75)
+    origImg = origImg[startLine: endLine]
 
-startLine = int(numLines * 0.25)
-endLine = int(numLines * 0.75)
-origImg = origImg[startLine: endLine]
+    imgCenter = (origImg.shape[0] / 2, origImg.shape[1] / 2)
+    img = cv2.GaussianBlur(origImg, (0, 0), 1)
 
-imgCenter = (origImg.shape[0] / 2, origImg.shape[1] / 2)
+    # apply thresholding to get eye shape
+    newImg = np.zeros(img.shape)
+    newImg[img < 44] = 1
 
-img = cv2.GaussianBlur(origImg, (0, 0), 1)
+    index = np.vstack(newImg.nonzero()) # 2xM matrix
+    index[(0, 1), :] = index[(1, 0), :]
+    index = index[:, index[1].argsort()[::-1]]
+    index = np.concatenate((index, np.ones((1, index.shape[1]), dtype=int)), axis=0)
 
-# convert origImg back to color
-origImg = cv2.cvtColor(origImg, cv2.COLOR_GRAY2RGB)
+    XmaxIdx = np.argmax(index[0])
+    XminIdx = np.argmin(index[0])
 
-newImg = np.zeros(img.shape)
-newImg[img < 44] = 1
+    result['left'] = index[:2, XminIdx]
+    result['right'] = index[:2, XmaxIdx]
 
-index = np.vstack(newImg.nonzero()) # 2xM matrix
+    diff = index[:, XmaxIdx] - index[:, XminIdx]
+    angle = math.degrees(math.atan2(diff[1], diff[0]))
 
-index[(0, 1), :] = index[(1, 0), :]
+    # get top and bottom eye limits from rotated view
+    rotation = cv2.getRotationMatrix2D(imgCenter, angle, 1)
+    rotIdx = rotation.dot(index)
 
-index = index[:, index[1].argsort()[::-1]]
-index = np.concatenate((index,np.ones((1,index.shape[1]),dtype=int)), axis=0)
+    rotImg = cv2.warpAffine(img, rotation, img.shape[::-1])
+    yMax = np.max(rotIdx[1])
+    yMin = np.min(rotIdx[1])
 
-XmaxIdx = np.argmax(index[0])
-XminIdx = np.argmin(index[0])
+    rotXavg = (rotIdx[0, XmaxIdx] + rotIdx[0, XminIdx]) / 2.
+    yOrigPoints = np.array([
+        [rotXavg, rotXavg],
+        [yMin, yMax],
+        [1, 1],
+    ])
 
-diff = index[:, XmaxIdx] - index[:, XminIdx]
+    yPoints = cv2.getRotationMatrix2D(imgCenter, -angle, 1).dot(yOrigPoints)
+    result['bottom'] = yPoints[:, 1]  # use yMax since y-axis increases downwards
+    result['top'] = yPoints[:, 0]
 
-angle = math.degrees( math.atan2(diff[1], diff[0] ))
+    # Hough circle detection
 
-print angle
-rotation = cv2.getRotationMatrix2D(imgCenter, angle, 1)
-rotIdx = rotation.dot(index)
+    # set a low threshold (param2) so that we are guaranteed at least 1. Set a high minimum distance (1000)
+    # so that we have at most 1
+    circles = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, 2, 1000, param1=30, param2=10, minRadius=10,
+                               maxRadius=15)
 
-rotImg = cv2.warpAffine(img, rotation, img.shape[::-1])
-yMax = np.max(rotIdx[1])
-yMin = np.min(rotIdx[1])
+    # virtually guaranteed one circle. raise an error otherwise
+    circle = circles[0, 0]
+    result['pupil'] = circle[0:2]
 
-rotXavg = (rotIdx[0, XmaxIdx] + rotIdx[0, XminIdx]) / 2.
-yOrigPoints = np.array([
-    [rotXavg, rotXavg],
-    [yMin, yMax],
-    [1,1],
-])
-yPoints = cv2.getRotationMatrix2D(imgCenter, -angle, 1).dot(yOrigPoints).astype(int)
+    if verbose:
 
-eyeBase = yPoints[:,1]  # use yMax since y-axis increases downwards
+        # convert origImg back to color
+        origImg = cv2.cvtColor(origImg, cv2.COLOR_GRAY2RGB)
 
-yOrigPoints = yOrigPoints[:2].astype(int)
-cv2.circle(rotImg, tuple(yOrigPoints[:,0]), 2, (0, 255, 0))
-cv2.circle(rotImg, tuple(yOrigPoints[:,1]), 2, (0, 255, 0))
+        print 'Eye angle:', angle
 
-keypoints = [
-    tuple(index[:2,XminIdx]),
-    tuple(index[:2,XmaxIdx]),
-    tuple(yPoints[:, 0]),
-    tuple(yPoints[:, 1]),
-]
+        yPoints = yPoints.astype(int)
 
-# Hough circle detection
+        keypoints = [
+            tuple(index[:2, XminIdx]),
+            tuple(index[:2, XmaxIdx]),
+            tuple(yPoints[:, 0]),
+            tuple(yPoints[:, 1]),
+        ]
 
-# set a low threshold (param2) so that we are guaranteed at least 1. Set a high minimum distance (1000)
-# so that we have at most 1
-circles = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, 2, 1000, param1=30, param2=10, minRadius=10,
-                                   maxRadius=15)
+        # draw keypoint in rotated image
+        yOrigPoints = yOrigPoints[:2].astype(int)
+        cv2.circle(rotImg, tuple(yOrigPoints[:, 0]), 2, (0, 255, 0))
+        cv2.circle(rotImg, tuple(yOrigPoints[:, 1]), 2, (0, 255, 0))
 
-# virtually guaranteed one circle. raise an error otherwise
-circle = circles[0,0]
-circleCenter = circle[0:2]
+        # draw pupil center
+        circle = np.around(circle)   # round for display only
+        cv2.circle(origImg, (circle[0], circle[1]), circle[2], (0, 255, 0), 1)
+        cv2.circle(origImg, (circle[0], circle[1]), 1, (0, 255, 0), 1)
 
-circle = np.around(circle)   # round for display only
-cv2.circle(origImg, (circle[0], circle[1]), circle[2], (0, 255, 0), 1)
-cv2.circle(origImg, (circle[0], circle[1]), 1, (0, 255, 0), 1)
+        # draw keypoints
+        for x in keypoints:
+            cv2.circle(origImg, x, 1, (255, 0, 0))
+        print keypoints
 
-# calculate vector relative to centered base of eye
-pupilVector = circleCenter - eyeBase
-print 'pupilVector = ', pupilVector
+        plt.subplot(221), plt.imshow(img, cmap=cm.Greys_r)
+        plt.subplot(222), plt.imshow(newImg)
+        plt.subplot(223), plt.imshow(rotImg)
+        plt.subplot(224), plt.imshow(origImg)
 
-print keypoints
-for x in keypoints:
-    cv2.circle(origImg, x, 1, (255, 0, 0))
+        plt.show()
 
+    return result
 
 
+if __name__ == "__main__":
 
-plt.subplot(221), plt.imshow(img, cmap=cm.Greys_r)
-plt.subplot(222), plt.imshow(newImg)
-plt.subplot(223), plt.imshow(rotImg)
-plt.subplot(224), plt.imshow(origImg)
+    if len(sys.argv) < 2:
+        print 'Usage: python ' + __file__ + ' <image>'
+        sys.exit()
 
-plt.show()
-
+    imgFile = sys.argv[1]
+    origImg = cv2.imread(imgFile, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    result = getEyeFeatures(origImg, True)
+    print result
--- extract_feature.py
+++ extract_feature.py
@@ -1,4 +1,61 @@
 from imports import *
+from detectEyeShape import getEyeFeatures
+
+
+def features(image, verbose):
+    feat = []
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # only process one face
+    if len(faces) == 0:
+        sys.exit()
+
+    (x, y, w, h) = faces[0]
+
+    feat += [x, y]
+
+    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    roi_gray = gray[y:y + h, x:x + w]
+    roi_color = image[y:y + h, x:x + w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+
+    eyes = eyes[eyes[:, 1].argsort()[:2]]  # take top 2 occurrences only
+    eyes = eyes[eyes[:, 0].argsort()]  # sort by x-axis
+
+    eyeNum = 0
+
+    for (ex, ey, ew, eh) in eyes:
+
+        feat.extend([ex, ey])
+
+        eyeNum += 1
+        if eyeNum == 2:
+            ew += 15
+
+        if verbose and len(sys.argv) > 2:
+            eyeColorImg = roi_color[ey:ey + eh, ex:ex + ew]
+            cv2.imwrite('testeye' + str(eyeNum) + '_' + sys.argv[2] + '.png', eyeColorImg)
+
+        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+        eyeGrayImg = roi_gray[ey:ey + eh, ex:ex + ew]
+        eyeFeat = getEyeFeatures(eyeGrayImg, False)
+
+        for v in eyeFeat.itervalues():
+            feat.extend(v.tolist())
+
+    roi_color = cv2.cvtColor(roi_color, cv2.COLOR_BGR2RGB)
+
+    if verbose:
+        plt.imshow(roi_color)
+        plt.show()
+
+    return feat
+
 
 if __name__ == "__main__":
 
@@ -8,93 +65,5 @@ if __name__ == "__main__":
 
     imgFile = sys.argv[1]
     image = cv2.imread(imgFile)
-
-    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    print "Found faces"
-
-    # only process one face
-    if len(faces) == 0:
-        sys.exit()
-
-    (x, y, w, h) = faces[0]
-    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    roi_gray = gray[y:y + h, x:x + w]
-    roi_color = image[y:y + h, x:x + w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    print "Found eyes"
-
-
-    eyes = eyes[eyes[:,1].argsort()[:2]]  # take top 2 occurrences only
-    eyes = eyes[eyes[:,0].argsort()]  # sort by x-axis
-
-    # blurImg = roi_gray
-    blurImg = cv2.medianBlur(roi_gray, 3)
-    # blurImg = cv2.GaussianBlur(roi_gray, (5,5), 4)
-
-    eyeNum = 0
-    eyeImage = np.zeros(( max(eyes[0][1], eyes[1][1]), eyes[0][0] + eyes[1][0], 3))
-    for (ex, ey, ew, eh) in eyes:
-
-        eyeNum += 1
-        if eyeNum == 2:
-            ew+=15
-        # detect pupils
-        eye_gray = blurImg[ey:ey + eh, ex:ex + ew]
-        eye_notblur = roi_color[ey:ey + eh, ex:ex + ew]
-
-        if len(sys.argv) > 2:
-            cv2.imwrite('testeye' + str(eyeNum) + '_' + sys.argv[2] + '.png', eye_notblur)
-
-        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-        eye_color = roi_color[ey:ey + eh, ex:ex + ew]
-
-        print np.min(eye_gray)
-        print np.max(eye_gray)
-
-        # set a low threshold (param2) so that we are guaranteed at least 1. Set a high minimum distance (1000)
-        # so that we have at most 1
-        circles = cv2.HoughCircles(eye_gray, cv2.cv.CV_HOUGH_GRADIENT, 2, 1000, param1=30, param2=10, minRadius=10,
-                                   maxRadius=15)
-
-        print circles
-        if circles is not None:
-            circles = np.around(circles)
-            for i in circles[0, :]:
-                cv2.circle(eye_color, (i[0], i[1]), i[2], (0, 255, 0), 1)
-                cv2.circle(eye_color, (i[0], i[1]), 2, (0, 0, 255), 1)
-
-    # newImg = cv2.resize(image, (960, 540))
-    # surf = cv2.SURF(400)
-    # surf.extended = True
-    # surf.upright = True
-    # kp, des = surf.detectAndCompute(blurImg, None)
-    # roi_color = cv2.drawKeypoints(roi_color, kp, None, (255, 0, 0), 4)
-
-    # edges = cv2.Canny(roi_gray, 30,50)
-    # cv2.imshow('image (press any key to quit)', roi_color)
-    # cv2.imshow('edges (press any key to quit)', edges)
-
-    roi_color = cv2.cvtColor(roi_color, cv2.COLOR_BGR2RGB)
-
-    # fig = plt.figure()
-    # a = fig.add_subplot(2, 2, 1)
-    # plt.imshow(roi_color)
-    #
-    # a = fig.add_subplot(2, 2, 2)
-    #
-    # a = fig.add_subplot(2, 2, 3)
-    #
-    # a = fig.add_subplot(2, 2, 4)
-    # plt.imshow(roi_gray, cmap=cm.Greys_r)
-    plt.imshow(roi_color)
-    plt.show()
-
-    # key = -1
-    # while key == -1:
-    #     key = cv2.waitKey(100)
-
-    # cv2.destroyAllWindows()
+    feat = features(image, True)
+    print feat
